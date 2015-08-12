1da2a75f1d79d2100a3ff9fae65ef650845db3c0
tqxmjb <958584313@qq.com>
2015-08-12 09:27:50 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+<script>
+    var cash;
+    cash = {
+      update:function(id,status){
+          $.ajax(
+              {
+                  type: "GET",
+                  url: '/shop/cash/update-status',
+                  data: {'id':id,'status':status},
+                  asynic: false,
+                  dataType: "json",
+                  beforeSend: function () {
+                  },
+                  success: function (result) {
+                      if(result['code']==1){
+                          var content = "<div style='width: 200px;text-align: center'>"+result['message']+"</div>"
+                          var d = dialog({
+                              title: "提示",
+                              content: content,
+                              ok: function () {
+                                  window.location.reload()
+                              }});
+                          d.showModal();
+                      } else {
+                          gf.alert(result['message']);
+                      }
+                  }
+              }
+          );
+      }
+    };
+</script>
